01978a9ccd913bc94f8ba642756111de37fa4acc
Laalasa Krishna Y <76587137+Laalasa@users.noreply.github.com>
2023-02-10 22:46:39 +0530

    Update Confetti.js

--- src/components/Confetti.js
+++ src/components/Confetti.js
@@ -2,7 +2,9 @@ import React from 'react'
 import useWindowSize from 'react-use/lib/useWindowSize'
 import Confetti from 'react-confetti'
 
-export default () => {
+
+
+export default function Confetti () {
   const { width, height } = useWindowSize()
   return (
     <Confetti
